f6fba13c5ecc3bd991fcf161068db4020ec0bc93
AdamNeuber <adam.neuber.024@pslib.cz>
2026-05-19 17:31:33 +0200

    almost finished features, small tweaks, status: i am not caffenaited enough for this

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 document.addEventListener("DOMContentLoaded", () => {
-    let hamelmnts = document.querySelectorAll(".hamburger-zone");
-    for (const btn of document.querySelectorAll(".hamburger")) {
-        btn.addEventListener("click", (e) => {
-            e.preventDefault();
-            for (const element of hamelmnts) {
-                element.classList.toggle("active");
-            }  
-        });
-    }
+  let hamelmnts = document.querySelectorAll(".hamburger-zone");
+  for (const btn of document.querySelectorAll(".hamburger")) {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      for (const element of hamelmnts) {
+        element.classList.toggle("active");
+      }
+    });
+  }
+});
+
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  modules: [Navigation],
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 });
